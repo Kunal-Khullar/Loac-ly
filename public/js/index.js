c4d886bb2056a5110395ae7396f4d6d8c4e08f6c
Kunal Khullar <kunalkhullar5012@gmail.com>
2021-02-19 18:53:42 +0530

    frontend half done

---
+++ public/js/index.js
@@ -0,0 +1,66 @@
+var s=1,t=1,u=1;
+function enable()
+{
+    var v1 = document.getElementById("placetype");
+    if(v1.value=="pg"){
+        document.getElementById("pgtype").disabled=false
+    }
+    else{
+        document.getElementById("pgtype").disabled=true
+    }
+}
+function toggleForm(){
+    document.body.classList.toggle('activeForm');
+  }
+function slide(){
+    console.log("harman noob")
+    if(s==1)
+    {
+        document.getElementById("slider").classList.remove("slideup")
+        document.getElementById("slider").classList.add("slidedown")
+        s=0;
+        document.getElementById("vd").innerHTML="Hide details"
+    }
+    else{
+        document.getElementById("slider").classList.add("slideup")
+        document.getElementById("slider").classList.remove("slidedown")
+        s=1;
+        document.getElementById("vd").innerHTML="View details"
+    }
+   
+}
+function slide2(){
+    console.log("harman noob")
+    if(t==1)
+    {
+        document.getElementById("slider2").classList.remove("slideup")
+        document.getElementById("slider2").classList.add("slidedown")
+        t=0;
+        document.getElementById("vd2").innerHTML="Hide details"
+    }
+    else{
+        document.getElementById("slider2").classList.add("slideup")
+        document.getElementById("slider2").classList.remove("slidedown")
+        t=1;
+        document.getElementById("vd2").innerHTML="View details"
+    }
+   
+}
+function slide3(){
+    console.log("harman noob")
+    if(u==1)
+    {
+        document.getElementById("slider3").classList.remove("slideup")
+        document.getElementById("slider3").classList.add("slidedown")
+        u=0;
+        document.getElementById("vd3").innerHTML="Hide details"
+    }
+    else{
+        document.getElementById("slider3").classList.add("slideup")
+        document.getElementById("slider3").classList.remove("slidedown")
+        u=1;
+        document.getElementById("vd3").innerHTML="View details"
+    }
+   
+}
+    
---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
+const path = require("path");
+app.set('view engine', 'hbs')
+app.set('views',path.join(__dirname,"../templates/views"));
+hbs.registerPartials(path.join(__dirname,"../templates/partials"));
+app.use(express.static(path.join(__dirname,"../public")));
+
+const port = process.env.PORT || 3000;
+app.get("/",(req,res)=>{
+    res.render('index');
+})
+app.get("/pg",(req,res)=>{
+    res.render("pg");
+})
+app.get("/jobs",(req,res)=>{
+    res.render("jobs")
+})
+app.listen(port,()=>{
+    console.log("Running")
+})
